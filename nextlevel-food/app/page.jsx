892d3506621d06ecf042c0e09b7a064b045e6923
lion772 <williamlopes64@gmail.com>
2024-01-01 15:40:41 +0100

    nextlevel food - page content - add assets, header and navbar, as well as header.module.css

--- nextlevel-food/app/page.jsx
+++ nextlevel-food/app/page.jsx
@@ -4,16 +4,6 @@ import Header from "@/components/header";
 export default function Home() {
   return (
     <main>
-        <Header />
-        <p>
-            <Link href="/meals">Meals</Link>
-        </p>
-        <p>
-            <Link href="/meals/share">Share Meal</Link>
-        </p>
-        <p>
-            <Link href="/community">Community</Link>
-        </p>
     </main>
   );
 }
--- nextlevel-food/components/header.jsx
+++ nextlevel-food/components/header.jsx
@@ -1,8 +1,35 @@
+import Link from "next/link";
+//Unlike in many other React projects, in NextJS you have to access 'src' property.
+import LogoImg from "@/assets/logo.png";
+import classes from './header.module.css';
+
 export default function Header() {
     return(
         <>
-            <h1>Welcome to my brand-new NextJS project!</h1>
-            <p>🔥 Let&apos;s get started! 🔥</p>
+            <header className={classes.header}>
+                <Link className={classes.logo} href="/">
+                    <img src={LogoImg.src} alt="A plate"/>
+                    NextLevel Food
+                </Link>
+
+                <nav className={classes.nav}>
+                    <ul>
+                        <li>
+                            <Link href="/meals">Meals</Link>
+                        </li>
+                        <li>
+                            <Link href="/meals/share">Share Meal</Link>
+                        </li>
+                        <li>
+                            <Link href="/community">Community</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <div className={classes.title}>
+                <h1>Welcome to my brand-new NextJS project!</h1>
+            </div>
         </>
-    )
+
+)
 }
