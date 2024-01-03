c79ab99240a5644e5bb3ab38ee5070d4c89873bf
lion772 <williamlopes64@gmail.com>
2024-01-03 22:00:35 +0100

    nextlevel food - caching - trigger cache revalidation

--- nextlevel-food/lib/actions.jsx
+++ nextlevel-food/lib/actions.jsx
@@ -2,12 +2,13 @@
 
 import {saveMeal} from "@/lib/meals";
 import {redirect} from "next/navigation";
+import {revalidatePath, revalidateTag} from "next/cache";
 
 export default async function shareMeal(prevState, formData) {
     const meal = {
         title: formData.get('title'),
         summary: formData.get('summary'),
-        instruction: formData.get('instruction'),
+        instructions: formData.get('instructions'),
         image: formData.get('image'),
         creator: formData.get('name'),
         creator_email: formData.get('email'),
@@ -20,13 +21,15 @@ export default async function shareMeal(prevState, formData) {
     }
 
     await saveMeal(meal);
+    //this function tells NextJS to revalidate (to throw away) the cache that belongs to a certain route path.
+    revalidatePath('/meals');
     redirect('/meals');
 }
 
 function isInvalidText(meal) {
     for (const key in meal) {
-        if (meal.hasOwnProperty(key)) {
-            if (!meal[key] || meal[key].trim() === '' || isEmailAndImageValid(meal)) {
+        if (meal.hasOwnProperty(key) && key !== 'image') {
+            if (!meal[key] || meal[key].trim() === ''|| isEmailAndImageValid(meal)) {
                 return true;
             }
         }
@@ -35,7 +38,7 @@ function isInvalidText(meal) {
 }
 
 function isEmailAndImageValid(meal) {
-    const isEmailInvalid = !meal.email || !meal.email.includes('@');
+    const isEmailInvalid = !meal.creator_email || !meal.creator_email.includes('@');
     const isImageInvalid = !meal.image|| meal.image.size === 0 ;
     return  isEmailInvalid || isImageInvalid;
 }
