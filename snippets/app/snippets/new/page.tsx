9153ce3426aa9ff49bfab91c9bad70496b6d16ba
lion772 <williamlopes64@gmail.com>
2024-01-05 23:15:01 +0100

    snippets - add useFormStatus in form, disable button when pending is truthy and show 'submitting' text, 'invalid input' display state.message from server-side provided there's empty input field

--- snippets/app/snippets/new/page.tsx
+++ snippets/app/snippets/new/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { useFormState } from 'react-dom';
+import { useFormState, useFormStatus } from 'react-dom';
 import {createSnippet} from "@/db/actions";
 
 export default function SnippetCreatePage() {
     const [state, formAction] = useFormState(createSnippet, {message: ''});
+    const { pending } = useFormStatus();
+
     return (
         <form action={formAction}>
             <h3>Create a Snippet</h3>
@@ -19,10 +21,13 @@ export default function SnippetCreatePage() {
                 <div className='flex gap-4'>
                     <label className='w-12' htmlFor="code">Code</label>
                     <textarea name='code'
-                           className='border rounded p-2 w-full'
-                           id='code'/>
+                              className='border rounded p-2 w-full'
+                              id='code'/>
                 </div>
-                <button type='submit' className='rounded p-2 bg-blue-200'>Create</button>
+                <button disabled={pending} type='submit' className='rounded p-2 bg-blue-200 w-1/4 mx-auto'>
+                    {pending ? 'Submitting...' : 'Create'}
+                </button>
+                {state.message && <p>{state.message}</p>}
             </div>
         </form>
     )
